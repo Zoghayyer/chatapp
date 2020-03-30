75f66cddd7214dff9643b0a4df6fdae624494907
Ahmed <azzoghayyer@gmail.com>
2020-03-30 12:21:48 -0700

    Improve styles

--- src/routes/rooms/components/room/non-admin-message/non-admin-message.js
+++ src/routes/rooms/components/room/non-admin-message/non-admin-message.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import '../room.scss';
 
 const NonAdminMessage = ({ message, name }) => (
-  <div className="text-left ml-3 w-50">
+  <div className="text-left ml-3 float-left w-50">
     <div className="mb-3">
       <span className="message-container-left">{message}</span>
       <div className="user-color">{name}</div>
